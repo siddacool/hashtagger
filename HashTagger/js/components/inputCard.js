d1d1307ca92bce5fc481292a992b0c82d72d1f85
Siddhesh Mangela <sidthecool007@gmail.com>
2016-12-27 12:41:26 +0530

    font size adjustment according to char length

--- HashTagger/js/components/inputCard.js
+++ HashTagger/js/components/inputCard.js
@@ -9,6 +9,9 @@ tagInput.addEventListener('input', function () {
     // trim leading/trailing spaces
     inputVal.original = this.value.trim();
     
+    // set final value to blank for now
+    inputVal.finale = '';
+    
     // Separation based on commas and semicolons
     inputVal.commaSeparated = {};
     // Remove double quotes
@@ -34,23 +37,40 @@ tagInput.addEventListener('input', function () {
     // add commas
     inputVal.hashRevert.value = inputVal.hashRevert.ekh.split(" ").join(", ");
     
+    // set value for inputVal.finale
     if (inputVal.original !== '') {
+        // if already a hastag
         if (inputVal.original.startsWith('#')) {
-            tagOutput.value = inputVal.hashRevert.value;
+            inputVal.finale = inputVal.hashRevert.value;
         }
         else {
+            // if comma/semicolon separated
             if (inputVal.original.search(/[,;]/) != -1) {
-                tagOutput.value = inputVal.commaSeparated.value;
+                inputVal.finale = inputVal.commaSeparated.value;
             }
+            // space separated
             else {
-                tagOutput.value = inputVal.spaceSeparated.value;
+                inputVal.finale = inputVal.spaceSeparated.value;
             }
         }
     }
+    
+    // set font size based on string length (inputVal.finale)
+    if((inputVal.finale.length >= 40) && (inputVal.finale.length <= 85)){
+        cardHolder.setAttribute('data-font','medium');
+    }
+    else if((inputVal.finale.length >= 86) && (inputVal.finale.length <= 120)){
+        cardHolder.setAttribute('data-font','small');
+    }
+    else if(inputVal.finale.length >= 121){
+        cardHolder.setAttribute('data-font','tiny');
+    }
     else {
-        tagOutput.value = '';
+        cardHolder.removeAttribute('data-font');
     }
     
+    // assign final value
+    tagOutput.value = inputVal.finale;
 })
 
 //---
