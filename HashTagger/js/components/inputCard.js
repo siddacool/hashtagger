a9801bcaa7b1dd6a28cc5ee15566d176b4ade6ca
Siddhesh Mangela <sidthecool007@gmail.com>
2016-12-27 11:13:48 +0530

    algorithm correction

--- HashTagger/js/components/inputCard.js
+++ HashTagger/js/components/inputCard.js
@@ -5,20 +5,50 @@
 
 tagInput.addEventListener('input', function () {
     var inputVal = {};
-    inputVal.original = this.value;
-    inputVal.noQuotes = inputVal.original.replace(/["]/g, "");
-    inputVal.noSpace = inputVal.noQuotes.replace(/\s/g, "");
-    inputVal.noComma = inputVal.noSpace.replace(/[,;]/g, " ");
-    inputVal.splitComma = inputVal.noSpace.split(/[,;]/).join(" #");
-    inputVal.addTag = '#' + inputVal.splitComma;
-    inputVal.removeHash = inputVal.original.replace(/[#]/g, "");
-    inputVal.splitSpace = inputVal.removeHash.split(" ").join(", ");
+     
+    // trim leading/trailing spaces
+    inputVal.original = this.value.trim();
     
-    if(inputVal.original.startsWith('#')){
-        tagOutput.value = inputVal.splitSpace;
+    // Separation based on commas and semicolons
+    inputVal.commaSeparated = {};
+    // Remove double quotes
+    inputVal.commaSeparated.ekh = inputVal.original.replace(/["]/g, "");
+    // Remove all white spaces
+    inputVal.commaSeparated.doh = inputVal.commaSeparated.ekh.replace(/\s/g, "");
+    // replace commas/semicolons with hashtags
+    inputVal.commaSeparated.tin = inputVal.commaSeparated.doh.split(/[,;]/).join(" #");
+    // add hastag at the begining
+    inputVal.commaSeparated.value = '#' + inputVal.commaSeparated.tin;
+    
+    // Separation based on Spaces
+    inputVal.spaceSeparated = {};
+    // replace spaces with hashtags
+    inputVal.spaceSeparated.ekh = inputVal.original.split(" ").join(" #");
+    // add hastag at the begining
+    inputVal.spaceSeparated.value = '#' + inputVal.spaceSeparated.ekh;
+    
+    // convert hastags to comma separated text
+    inputVal.hashRevert = {};
+    // remove hastags
+    inputVal.hashRevert.ekh = inputVal.original.replace(/[#]/g, "");
+    // add commas
+    inputVal.hashRevert.value = inputVal.hashRevert.ekh.split(" ").join(", ");
+    
+    if (inputVal.original !== '') {
+        if (inputVal.original.startsWith('#')) {
+            tagOutput.value = inputVal.hashRevert.value;
+        }
+        else {
+            if (inputVal.original.search(/[,;]/) != -1) {
+                tagOutput.value = inputVal.commaSeparated.value;
+            }
+            else {
+                tagOutput.value = inputVal.spaceSeparated.value;
+            }
+        }
     }
-    else{
-        tagOutput.value = '#' + inputVal.splitComma;
+    else {
+        tagOutput.value = '';
     }
     
 })
