760688a2d3f7809ecc18181c7c7d37f615d92666
Siddhesh Mangela <sidthecool007@gmail.com>
2016-12-30 14:15:16 +0530

    aboutPopup animation

--- HashTagger/js/components/aboutPop.js
+++ HashTagger/js/components/aboutPop.js
@@ -5,12 +5,20 @@
 
 //## Launch about onclick title
 title.addEventListener('click',function() {
-    aboutPopup.classList.add('show');
+    header.classList.add('hide');
+    cardHolder.classList.add('expand');
+    setTimeout(function(){
+        aboutPopup.classList.add('show');
+    }, 200);
 });
 
 //## Back to homepage
 backButtonAboutPopup.addEventListener('click',function(){
     aboutPopup.classList.remove('show');
+    setTimeout(function(){
+        cardHolder.classList.remove('expand');
+        header.classList.remove('hide');
+    }, 200);
 })
 
 //---
